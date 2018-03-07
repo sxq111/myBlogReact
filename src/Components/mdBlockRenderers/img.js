328e7c92d9a657bbdf46484fa2e03123ed7ed0bc
sxq222 <18840830439@163.com>
2018-03-07 14:59:17 +0800

    md图片显示

--- src/Components/mdBlockRenderers/img.js
+++ src/Components/mdBlockRenderers/img.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default class extends Component {
 
     render() {
+        //本地图片
         let theImg = null;
         if (/\\src\\articles/.test(this.props.src)) {
             let path = (this.props.src.split('\\src\\')[1]);
@@ -11,6 +12,9 @@ export default class extends Component {
             path = path.join('/').split('?')[0];
             theImg = require(`../../articles/${path}`);
             console.log(path);
+        }else{
+            //网络图片
+            theImg = this.props.src;
         }
         return (
             <img alt = '暂无图片' src = {theImg} />
